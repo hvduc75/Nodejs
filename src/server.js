a2e3116697e041f8bc88985c40ed413b29c87c33
Hoang Van Duc <duch52362@gmail.com>
2024-07-24 22:53:39 +0700

    #31 Use MySQL with Nodejs

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
-const webRoutes = require('./routes/web')
+const webRoutes = require('./routes/web');
+const mysql = require('mysql2');
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
@@ -13,6 +14,24 @@ configViewEngine(app);
 // khai báo route
 app.use('/', webRoutes);
 
+// test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, // default: 3306
+    user: 'root', // default: empty
+    password: '123456',
+    database: 'hoidanit',
+});
+
+// simple query
+connection.query(
+  'select * from Users u',
+  function (err, results, fields) {
+    console.log(">>> results = ", results);
+    console.log(">>> fields = ", fields)
+  }
+)
+
 app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
