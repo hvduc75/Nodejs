4db8bfe6ee90aebe3d973fd5a4d869e79174bb92
Hoang Van Duc <duch52362@gmail.com>
2024-07-24 18:40:14 +0700

    #20 install nodemon

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
+
+// config template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+// khai báo route
+app.get('/', (req, res) => {
+  res.send('Hello World! & hoang van duc')
+})
+
+app.get('/hoidanit', (req, res) => {
+  res.render('sample.ejs')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on port ${port}`)
+})
