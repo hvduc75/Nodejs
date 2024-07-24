adfa2d4b0e0c058013f5030f0ce4a558bf1a64ab
Hoang Van Duc <duch52362@gmail.com>
2024-07-24 21:06:31 +0700

    #24 Use MVC

--- src/server.js
+++ src/server.js
@@ -1,23 +1,17 @@
-const express = require('express');
-const path = require('path');
 require('dotenv').config();
+const express = require('express');
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web')
 
 const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
 // config template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+configViewEngine(app);
 
 // khai báo route
-app.get('/', (req, res) => {
-  res.send('Hello World! & hoang van duc')
-})
-
-app.get('/hoidanit', (req, res) => {
-  res.render('sample.ejs')
-})
+app.use('/', webRoutes);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
