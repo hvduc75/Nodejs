19694579a430c1cde81064ec531864f15aaa2eab
Hoang Van Duc <duch52362@gmail.com>
2024-07-25 09:32:25 +0700

    #35 Design NavBar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,7 @@
 const connection = require('../config/database');
 
 const getHomePage = (req, res) => {
-    let users = [];
-    connection.query('select * from Users u', function (err, results, fields) {
-        users = results;
-        console.log('>>> results = ', results);
-        res.send(JSON.stringify(users));
-    });
+    return res.render('home.ejs')
 };
 
 const getABC = (req, res) => {
